d1ea150131614596b571a4a9ee5eacba5ce152bf
Daniel Connolly <Daniel.Connolly@dotdigital.com>
2023-02-27 12:52:37 +0000

    more changes

---
+++ PartitionCompressionIssue/PartitionCompressionIssue/Storage/pf_PartitionCompressionIssue.sql
@@ -0,0 +1,4 @@
+﻿CREATE PARTITION FUNCTION [pf_PartitionCompressionIssue](INT)
+    AS RANGE
+    FOR VALUES (1, 10, 100);
+
---
+++ SSDTXMLBug/SSDTXMLBug/ExampleSprocExecution.sql
@@ -0,0 +1,13 @@
+﻿DECLARE @SQLXML XML
+SET @SQLXML = '
+<teams>
+<dbateam>
+<team id="1">Daniel</team>
+<team id="2">Robert</team>
+<team id="3">Caroline</team>
+<team id="4">Alex</team>
+</dbateam>
+</teams>
+'
+
+exec [dbo].[xmlExample] @sqlxml
